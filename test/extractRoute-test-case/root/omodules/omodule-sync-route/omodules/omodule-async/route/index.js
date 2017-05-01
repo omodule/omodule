610f01b56c9cc24f729ca7a72394a5cda848afb0
wuchangming <wuchangmingnice@163.com>
2017-05-01 15:20:26 +0800

    delete: delete `test folder`

--- test/extractRoute-test-case/root/omodules/omodule-sync-route/omodules/omodule-async/route/index.js
+++
@@ -1,8 +0,0 @@
-const route = {
-    path: __omodule_foldername,
-    getComponent: (nextState, cb) => {
-        cb(null, require('../containers/BaseContainer').default);
-    }
-};
-
-export default route;
--- test/extractSyncReducer-test-case/root/omodules/omodule-temp/omodules/xxxxx/reducer/index.js
+++
@@ -1,3 +0,0 @@
-export default reducer = (state= {}, action) {
-    return state
-}
